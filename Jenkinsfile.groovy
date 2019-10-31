c3ddff7fe2dc70bce0cf4f575c55abefcfddfa5d
Anand Tiwari <Anand.Tiwari@coxautoinc.com>
2019-10-31 14:45:12 -0400

    jenkins file changes

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,10 @@ pipeline{
   agent any
   stages {
     stage('RUN') {
-      echo 'This stage will be executed first.'
+      echo 'This stage will be executed first run.'
+    }
+    stage('RUN 2') {
+      echo 'This stage will be executed second run.'
     }
   }
 }
